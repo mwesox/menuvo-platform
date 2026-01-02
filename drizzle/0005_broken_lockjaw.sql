f7ac10ec636d08715b806d4f0b9ecf10d57db357
Matthias Wesolowski <mw@wedecom-consulting.de>
2026-01-02 22:41:52 +0100

    Reset migrations - regenerate from clean schema

--- drizzle/0005_broken_lockjaw.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE "option_choices" ADD COLUMN "is_default" boolean DEFAULT false NOT NULL;--> statement-breakpoint
-ALTER TABLE "option_choices" ADD COLUMN "min_quantity" integer DEFAULT 0 NOT NULL;--> statement-breakpoint
-ALTER TABLE "option_choices" ADD COLUMN "max_quantity" integer;--> statement-breakpoint
-ALTER TABLE "option_groups" ADD COLUMN "type" text DEFAULT 'multi_select' NOT NULL;--> statement-breakpoint
-ALTER TABLE "option_groups" ADD COLUMN "num_free_options" integer DEFAULT 0 NOT NULL;--> statement-breakpoint
-ALTER TABLE "option_groups" ADD COLUMN "aggregate_min_quantity" integer;--> statement-breakpoint
-ALTER TABLE "option_groups" ADD COLUMN "aggregate_max_quantity" integer;
